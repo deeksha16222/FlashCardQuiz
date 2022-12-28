7558b05d784b709276d0dab0a3fa8ff4c2a65228
Deeksha1998 <deekshabisht16@gmail.com>
2022-12-28 15:49:36 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
+import "./App.css";
+import React,{useState} from 'react'
+import List from "./List"
 
 function App() {
+  const[flashcards,setFlashCards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <List flashcards={flashcards}/>
     </div>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "What is 2+2?",
+    answer: "4",
+    options: ["2", "3", "4", "5"],
+  },
+  {
+    id: 2,
+    question: "What is question?",
+    answer: "answer2",
+    options: ["2", "3", "4", "5"],
+  },
+  {
+    id: 3,
+    question: "What is question?",
+    answer: "answer3",
+    options: ["2", "3", "4", "5"],
+  },
+];
 
 export default App;
